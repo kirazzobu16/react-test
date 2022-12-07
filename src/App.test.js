3e5063dda95c86c8364f4aca80989392399390bc
Burak Kiraz <kirazzobu16@gmail.com>
2022-12-07 13:02:56 +0300

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render,screen,fireEvent, getByText} from "@testing-library/react";
+import userEvent from "@testing-library/user-event"
+import App from "./App";
+import '@testing-library/jest-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("List test",()=>{
+    let Emoji,text,input
+
+    beforeEach(()=>{
+      render(<App/>)
+      text= screen.getByPlaceholderText("Emoji Adı Giriniz")
+      Emoji=screen.getAllByTestId("Copy")
+      
+    })
+
+    test('HeaderEmoji', () => { 
+         expect(text).toBeInTheDocument()
+
+     })
+     
+     test("Emoji", () => { 
+         expect(Emoji.length).toEqual(20)
+     })
+
+     test("EmojiSearch",()=>{
+         const Name="Kissing"
+         const EmojiType=screen.getByRole("textbox")
+         userEvent.type(EmojiType,Name)
+         expect(screen.getByText(Name)).toBeInTheDocument()
+         
+     })
+
+     test('copy', () => { 
+        expect(Emoji[0]).toHaveAttribute("data-clipboard-text");
+    });
+})
